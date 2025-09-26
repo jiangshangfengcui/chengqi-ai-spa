9a849ece7ec9568fc381cb394c611d3118c6558d
jiangshangfengcui <jiangshangfengcui@126.com>
2025-09-26 22:05:20 +0800

    feat: 修改CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    // baseUrl: 'http://localhost:3000', // 替换为你的应用地址
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    video: false, // 是否记录视频
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
